c1a88d5d44217d368e6d2ba1d8b3cbd5d34a5c00
JannikBrack <jannik.brack@icloud.com>
2026-07-24 19:22:21 +0200

    Fix LED signal frequency to 400000 Hz for compatibility

--- led_control.py
+++ led_control.py
@@ -6,7 +6,7 @@ from rpi_ws281x import PixelStrip, Color
 # LED strip configuration
 LED_COUNT = 100        # Number of LED pixels
 LED_PIN = 18           # GPIO pin connected to the pixels (18 uses PWM!)
-LED_FREQ_HZ = 800000   # LED signal frequency in hertz (usually 800khz)
+LED_FREQ_HZ = 400000   # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10           # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 128   # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False     # True to invert the signal (when using NPN transistor level shift)
