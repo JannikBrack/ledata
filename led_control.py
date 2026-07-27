1b170b46912776e30943be921b0fd8c802a65819
JannikBrack <jannik.brack@icloud.com>
2026-07-27 18:56:36 +0200

    Fix LED_FREQ_HZ configuration to correct signal frequency for LED strip

--- led_control.py
+++ led_control.py
@@ -6,7 +6,7 @@ import argparse
 LED_COUNT      = 300     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_FREQ_HZ    = 400000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating a signal (try 10)
 LED_BRIGHTNESS = 65      # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
